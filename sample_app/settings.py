39c764cdfac789017f71a4562addbef7fe8ab983
syutuial <noklot8er@gmail.com>
2019-05-05 16:23:52 +0900

    fisrt commit#

--- sample_app/settings.py
+++ sample_app/settings.py
@@ -127,3 +127,15 @@ db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+DEBUG = False
+
+try:
+    from.local_settings import *
+except ImportError:
+    pass
+
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
+
